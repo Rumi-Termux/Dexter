6be3995076037f97297d7ef44eef5d0274848ff0
Rumi-Termux <isabozanun.21@gmail.com>
2026-08-12 17:05:00 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const http = require('http');
+http.createServer((req, res) => {
+  res.write("Bot 2 aktif!");
+  res.end();
+}).listen(process.env.PORT || 3000);
+
+const mineflayer = require('mineflayer')
+const bot = mineflayer.createBot({
+  host: 'mc.muzcraft.com',
+  username: 'JENNIE',
+  version: '1.12.2',
+  hideErrors: true
+});
+
+// CHUNK YAZILARINI SUSTURAN GÜVENLİ YOL
+bot.on('kicked', console.log)
+bot.on('error', console.log)
+
+bot.on('message', (msg) => {
+  const m = msg.toString();
+  console.log("SUNUCU: " + m);
+
+  if (m.includes('/login') || m.includes('/gir')) {
+    bot.chat('/login jeniekim');
+  }
+
+  if (m.includes('Başarıyla giriş yaptınız') || m.includes('Lobiye bağlandınız')) {
+    setTimeout(() => bot.chat('/skyblock'), 5000);
+  }
+
+  if (m.includes('SkyBlock') && !m.includes('Sunucusuna bağlanıyorsunuz')) {
+    setTimeout(() => bot.chat('/is go'), 10000);
+  }
+});
+
+setInterval(() => {
+  if (bot.entity) {
+    bot.setControlState('jump', true);
+    setTimeout(() => bot.setControlState('jump', false), 500);
+  }
+}, 15000);
+
+bot.on('end', () => setTimeout(() => process.exit(), 5000));
+
